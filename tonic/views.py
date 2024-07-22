1840c043d34e01b37cb777a6d97ea2561f88d60a
Daniel Maina <danielmaina989@gmail.com>
2024-07-22 23:26:50 +0300

    chords

--- tonic/views.py
+++ tonic/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render (request, 'pages/index.html',{'name':'Dantey'})
+
+def add(request):
+    val1 = int(request.GET["num1"])
+    val2 = int(request.GET["num2"])
+    results = val1 +  val2
+    return render(request, "results.html", {'results':results})
