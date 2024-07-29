976cb5272c5a607a7604d60f30427e2b082f3a15
Daniel Maina <danielmaina989@gmail.com>
2024-07-29 23:55:53 +0300

    created a model Destinantion

--- travellor/views.py
+++ travellor/views.py
@@ -7,18 +7,22 @@ def index(request):
     dest1.desc = "The city that never sleeps"
     dest1.img = 'destination_1.jpg'
     dest1.price = 700
+    dest1.offer = False
 
     dest2 = Destination()
     dest2.name = "Mombasa"
     dest2.desc = "The veauty of sea and beaches"
     dest2.img = 'destination_2.jpg'
     dest2.price = 1500
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.name = "Kisumu"
     dest3.desc = "Enjoy the fish and cast culture"
     dest3.img = 'destination_3.jpg'
     dest3.price = 1000
+    dest3.offer = False
+
 
     dests = [dest1, dest2 , dest3]
 
