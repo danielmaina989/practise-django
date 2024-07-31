3f3d2325d897438cd9ffd4fc721bf9521c39b377
Daniel Maina <danielmaina989@gmail.com>
2024-07-31 23:39:59 +0300

    added user reistration form

--- chords/settings.py
+++ chords/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'travellor.apps.TravellorConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- chords/urls.py
+++ chords/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('travellor.urls')),
+     path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
